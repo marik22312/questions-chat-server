351f09fe3f143d675f26a52233339ca96d6b36e0
mark22312 <mark22312@gmail.com>
2020-03-29 19:44:32 +0300

    Opened socket

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import express from 'express';
+import nativeHttpDriver from 'http';
+import socketIo from 'socket.io';
 
-const server = express();
+const app = express();
 
-server.listen(process.env.PORT || 3000);
+const httpServer = new nativeHttpDriver.Server(app);
+const io = socketIo(httpServer);
+httpServer.listen(3000);
+// WARNING: app.listen(80) will NOT work here!
 
-server.get('/', (req, res) => {
-	res.send('Omer Ya Ashah!');
-})
+app.get('/', function (req, res) {
+  res.send('Omer Ya Beiza');
+});
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
